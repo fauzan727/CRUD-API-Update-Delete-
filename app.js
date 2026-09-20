179b08ef7c0d2f876ae1a457349c818ebaaf59a1
fauzan727 <fauzanhafizhzulfikar@gmail.com>
2026-09-20 22:56:21 +0700

    perbaikan

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -13,14 +15,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
-
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api', userRoutes); 
 
-app.use(express.static(fileURLToPath(new URL('../frontend/', import.meta.url))));
+app.use(express.static(fileURLToPath(new URL('../marketplace-frontend/', import.meta.url))));
+
+app.get('*any', (req, res) => {
+  if (!req.path.startsWith('/api')) {
+    res.sendFile(fileURLToPath(new URL('../marketplace-frontend/index.html', import.meta.url)));
+  }
+});
+
 app.listen(3000, '127.0.0.1', () => {
   console.log('Buka http://localhost:3000');
 });
